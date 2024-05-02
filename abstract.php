0693c3114fdb60ba5387453af5100fb70c6d2016
Nirobtpi <saifasabanirob@gmail.com>
2024-05-02 20:36:04 +0600

    interface

--- abstract.php
+++ abstract.php
@@ -19,5 +19,35 @@
 
  $obj=new ChildClass;
  $obj->someMethod2('Mango','Red');
+ echo "<br>";
 
+ abstract class Car{
+   public $name;
+    function __construct($name)
+   {
+      $this->name=$name;
+   }
+   abstract function intro();
+ }
+
+ class Audi extends Car{
+
+   function intro()
+   {
+      return "Choose German quality! I'm an $this->name!";
+   }
+ }
+$audi=new Audi('Audi');
+echo $audi->intro();
+echo "<br>";
+
+class Bmw extends Car{
+
+   function intro()
+   {
+      return "Choose India quality! I'm an $this->name";
+   }
+}
+$bmw=new Bmw('Bmw');
+echo $bmw->intro();
 ?>
--- interface.php
+++ interface.php
@@ -22,6 +22,39 @@ $obj2=new Nirob;
 $obj2->name();
 echo "<br>";
 $obj2->nid();
+echo "<br>";
+
+interface Animal{
+    function makeSound();
+    
+}
+
+class Cat implements Animal{
+    function makeSound()
+    {
+        echo "Meow <br>";
+    }
+}
+class Dog implements Animal{
+    function makeSound()
+    {
+        echo "Bark <br>";
+    }
+}
+class Mouse implements Animal{
+    function makeSound()
+    {
+        echo "Squeak <br>";
+    }
+}
+$cat=new Cat;
+$dog=new Dog;
+$mouse=new Mouse;
+$animals=array($cat,$dog,$mouse);
+
+foreach($animals as $animal){
+    $animal->makeSound();
+}
 
 
 ?>
